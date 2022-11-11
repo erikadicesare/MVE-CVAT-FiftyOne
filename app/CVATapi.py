1e72a8ee18e2c8137ef3c251bef996481cc32275
Musa <musausr@CN-PC.cnigroup.net>
2022-11-11 17:26:35 +0100

    migliorata home e salvataggio immagine copertina project

--- app/CVATapi.py
+++ app/CVATapi.py
@@ -4,6 +4,8 @@ import os, json
 from pathlib import Path
 import ntpath
 import shutil
+from PIL import Image  
+import PIL  
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,19 +15,21 @@ password = os.getenv('PASSWORD_CVAT')
 url_cvat = os.getenv('URL_CVAT')
 
 max_size_load_file = os.getenv('MAX_SIZE_LOAD_FILE')
-credentials = {
-    "username": username,
-    "email": "",
-    "password": password
-}
 
-login = requests.post('{}/auth/login'.format(url_cvat), json= credentials)
-jsonObj = json.loads(login.text)
-keyLogin = jsonObj['key']
-print(keyLogin)
 
-""""""
+
 def uploadImages(id, name_task, files):
+    credentials = {
+        "username": username,
+        "email": "",
+        "password": password
+    }
+
+    login = requests.post('{}/auth/login'.format(url_cvat), json= credentials)
+    jsonObj = json.loads(login.text)
+    keyLogin = jsonObj['key']
+    print(keyLogin)
+
     uuid = request.form.get('uuid')
         
     pathFolder = 'temp{}'.format(uuid)
@@ -50,35 +54,9 @@ def uploadImages(id, name_task, files):
     
     createEmptyTask = requests.post('{}/tasks'.format(url_cvat), data=dataTask, headers={"Authorization": f'Token {keyLogin}'})
     jsonObj = json.loads(createEmptyTask.text)
+    #if (createEmptyTask.text == '{"detail":"You do not have permission to perform this action."}'):
     taskId = jsonObj['id']
     print(taskId)
-    
-    ############################################################################################################################################
-    
-    # Salvo il primo file dentro static/data/projectCVAT per avere una immagine significativa per progetto (immagine presa dall'ultimo task inserito)
-    #image = files
-    #headfp, tailfp = ntpath.split(image[0].filename)
-    #file_name, file_extension = ntpath.splitext(image[0].filename)
-    #pathFirstFile = './app/static/data/projectCVAT/{}{}'.format(taskId, file_extension)
-    #image[0].save(pathFirstFile)
-    
-    ############################################################################################################################################
-    
-    # Elimino il precedente file salvato per questo progetto (cerco i task e cerco gli id nella cartella static/data/projectCVAT)
-    #prj = requests.get('http://localhost:8080/api/v1/projects/{}'.format(id), headers={"Authorization": f'Token {keyLogin}'})
-    #jsonObj = json.loads(prj.text)
-    #tasks_id = []
-    #for t in jsonObj['tasks']:
-    #    tasks_id.append(t['id'])
-    #images_from_dir = os.listdir('./app/static/data/projectCVAT')
-    #for ifd in images_from_dir:
-    #    file_name, file_extension = ntpath.splitext(ifd)
-    #    print(file_name, file_extension)
-    #    print(tasks_id)
-    #    if ((file_name in tasks_id) and (file_name!=tasks_id)):
-    #        #img = "data/projectCVAT/{}{}".format(file_name, file_extension)
-    #        print("immagine {} esistente".format(file_name))
-    ############################################################################################################################################
     
     fs = []
     size = 0 
@@ -90,6 +68,8 @@ def uploadImages(id, name_task, files):
         
         pathFile = 'temp{}/'.format(uuid) + tailfp
         file.save(pathFile)
+        print("PATHFILE image")
+        print(file)
         totalSize = totalSize + Path(pathFile).stat().st_size
     
     #nTasks = round_up(totalSize/int(max_size_load_file))
@@ -120,10 +100,31 @@ def uploadImages(id, name_task, files):
         images = {f'client_files[{i}]': open(f, 'rb') for i, f in enumerate(fs)}
         uploadImgs = requests.post('{}/tasks/{}/data'.format(url_cvat,taskId), data={'image_quality':100}, files=images, headers={"Authorization": f'Token {keyLogin}'})
         print(uploadImgs.text)
+
+    #################################################################################################################################################
+    
+    # Salvo il primo file dentro static/data/projectCVAT per avere una immagine significativa per progetto (immagine presa dall'ultimo task inserito)
+
+    picture = Image.open(fs[0])
+    file_name, file_extension = ntpath.splitext(fs[0])
+    pathFirstFile = './app/static/data/projectCVAT/{}{}'.format(taskId, file_extension)
+    picture = picture.save(pathFirstFile)
+
+    #################################################################################################################################################
     
     shutil.rmtree('temp{}'.format(uuid))
 
 def create_project(name_project):
+    credentials = {
+        "username": username,
+        "email": "",
+        "password": password
+    }
+
+    login = requests.post('{}/auth/login'.format(url_cvat), json= credentials)
+    jsonObj = json.loads(login.text)
+    keyLogin = jsonObj['key']
+    print(keyLogin)
 
     dataProject = {
         "name": name_project
@@ -135,16 +136,29 @@ def create_project(name_project):
     return projectId
 
 def get_project(id_prj_MVE, id_prj_CVAT):
+    credentials = {
+        "username": username,
+        "email": "",
+        "password": password
+    }
+
+    login = requests.post('{}/auth/login'.format(url_cvat), json= credentials)
+    jsonObj = json.loads(login.text)
+    keyLogin = jsonObj['key']
+    print(keyLogin)
+
     prj = requests.get('{}/projects/{}'.format(url_cvat, id_prj_CVAT), headers={"Authorization": f'Token {keyLogin}'})
     jsonObj = json.loads(prj.text)
     img = "data/projectCVAT/notasks.png"
     nImages = 0
     if (len(jsonObj['tasks']) != 0):
-        
+        print(prj.text)
         tasks_id = []
         for t in jsonObj['tasks']:
-            tasks_id.append(t['id'])
-            nImages = nImages+t['size']
+            tasks_id.append(t)
+            prj = requests.get('{}/tasks/{}'.format(url_cvat, t), headers={"Authorization": f'Token {keyLogin}'})
+            jsonObjtask = json.loads(prj.text)
+            nImages = nImages+jsonObjtask['size']
         files = os.listdir('./app/static/data/projectCVAT')
         for file in files:
             file_name, file_extension = ntpath.splitext(file)
