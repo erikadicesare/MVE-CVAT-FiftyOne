ff85276a2905d2793fa61b68f4f74098e060034e
Musa <musausr@CN-PC.cnigroup.net>
2022-12-05 16:00:57 +0100

    add file to set database the first time

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ If an error like "Max retries exceeded with url" occurred, run instead
 `$ flask run --host=0.0.0.0 --port=<num_port>`
 
 where "num_port" is a port different from 5000 (default one)
+
+
+N.B. the file named "db_first_setup.py" (inside "app" folder) if runned with the command `python db_first_setup.py` will create a database named "MVE Database" with all the tables needed
+N.B.2 the file .env has to be set or create if doesn't exist before running the command `flask run`
+
+.env content:
+`USERNAME_CVAT=<cvat_username>`
+`PASSWORD_CVAT=<cvat_password>`
+`URL_CVAT=http://<your_host_with_port>/api #change it with the host of your cvat server`
+`MAX_SIZE_LOAD_FILE=1000000000`
+`USER_DB=root #change it with the user if different from root`
+`NAME_DB=MVE Database`
---
+++ app/db_first_setup.py
@@ -0,0 +1,135 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user_db = os.getenv('USER_DB')
+name_db = os.getenv('NAME_DB')
+#pw_db = os.getenv('PW_DB')
+
+params = {
+    'user': '', ##### insert the name of the user 
+    'password': '', #### insert the password
+}
+
+mydb = mysql.connector.connect(**params)
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE `MVE Database`")
+
+params = {
+    'user': '', ##### insert the name of the user 
+    'password': '', #### insert the password
+    'database': 'MVE Database'
+}
+
+def create_ProjectsMVE():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = "CREATE TABLE ProjectsMVE (IdProjectMVE BIGINT(20) NOT NULL AUTO_INCREMENT, NomeProgetto VARCHAR(100), Descrizione MEDIUMTEXT, ImmagineDescrittiva varchar(100), PRIMARY KEY (IdProjectMVE));" 
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+def create_ProjectMVExProjectCVAT():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = """
+            CREATE TABLE ProjectMVExProjectCVAT 
+            (IdProjectMVE BIGINT(20) NOT NULL, 
+            IdProjectCVAT BIGINT(20) NOT NULL, 
+            PRIMARY KEY (IdProjectMVE, IdProjectCVAT), 
+            FOREIGN KEY (IdProjectMVE) REFERENCES ProjectsMVE(IdProjectMVE) ON DELETE CASCADE ON UPDATE CASCADE);
+        """
+
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+def create_Truth():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = """
+            CREATE TABLE Truth 
+            (IdSample BIGINT(20) NOT NULL AUTO_INCREMENT, 
+            IdProjectMVE BIGINT(20) NOT NULL,
+            Name VARCHAR(100) NOT NULL, 
+            PRIMARY KEY (IdSample), 
+            FOREIGN KEY (IdProjectMVE) REFERENCES ProjectsMVE(IdProjectMVE) ON DELETE CASCADE ON UPDATE CASCADE);
+        """
+        
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+def create_TruthValues():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = """
+            CREATE TABLE TruthValues 
+            (Id BIGINT(20) NOT NULL AUTO_INCREMENT,
+            PropName VARCHAR(100),
+            ValueReal DOUBLE,
+            ValueString VARCHAR(100),
+            IdTruth BIGINT(20) NOT NULL,
+            PRIMARY KEY (Id), 
+            FOREIGN KEY (IdTruth) REFERENCES Truth(IdSample) ON DELETE CASCADE ON UPDATE CASCADE);
+        """
+        
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+def create_PredList():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = """
+            CREATE TABLE PredList 
+            (Id BIGINT(20) NOT NULL AUTO_INCREMENT,
+            IdPrediction VARCHAR(100) NOT NULL,
+            Name VARCHAR(100) NOT NULL,
+            IdProjectMVE BIGINT(20) NOT NULL,
+            PRIMARY KEY (Id), 
+            FOREIGN KEY (IdProjectMVE) REFERENCES ProjectsMVE(IdProjectMVE) ON DELETE CASCADE ON UPDATE CASCADE);
+        """
+        
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+def create_MVSxCVAT():
+    mydb = mysql.connector.connect(**params)
+    mycursor = mydb.cursor()
+
+    sql = """
+            CREATE TABLE MVSxCVAT 
+            (IdCVAT BIGINT(20) NOT NULL,
+            IdProjectMVE BIGINT(20) NOT NULL,
+            IdTask VARCHAR(100) NOT NULL,
+            PRIMARY KEY (IdCVAT, IdProjectMVE), 
+            FOREIGN KEY (IdProjectMVE) REFERENCES ProjectsMVE(IdProjectMVE) ON DELETE CASCADE ON UPDATE CASCADE);
+        """
+        
+    mycursor.execute(sql)
+
+    mycursor.close()
+    mydb.close()
+
+
+create_ProjectsMVE()
+create_ProjectMVExProjectCVAT()
+create_Truth()
+create_TruthValues()
+create_PredList()
+create_MVSxCVAT()
