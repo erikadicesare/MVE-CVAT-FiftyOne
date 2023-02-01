dc8661bdb19288039e437bce85fc88b4ca8f3078
Musa <musausr@CN-PC.cnigroup.net>
2023-02-01 14:26:57 +0100

    copy images puntalini

--- app/copyImg.py
+++ app/copyImg.py
@@ -1,6 +1,92 @@
+import random
 import shutil
 
-source = '/home/musausr/images/cvat/Data_0_01_00_02_12.bmp'
-for i in range(0,199):
-    target = '/home/musausr/images/cvat/Data_0_01_00_02_12_{}.bmp'.format(i)
+source = '/home/musausr/puntalini/Puntalini/202212011416143083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161430{}___Data_1_1_0_2_0.bmp'.format(ran)
     shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416153083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161530{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416163083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161630{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416173083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161730{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416183083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161830{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416193083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114161930{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416203083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114162030{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416213083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114162130{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
+
+source = '/home/musausr/puntalini/Puntalini/202212011416223083___Data_1_1_0_2_0.bmp'
+randoms = []
+for i in range(0,10):
+    ran = random.randint(10,99) 
+    while ran in randoms:
+        ran = random.randint(10,99) 
+    target = '/home/musausr/puntalini/Puntalini/2022120114162230{}___Data_1_1_0_2_0.bmp'.format(ran)
+    shutil.copy(source, target)
+    randoms.append(ran)
